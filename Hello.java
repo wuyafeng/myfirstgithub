d50dc5941c436d398f44d003d88cd0a86dec0d0a
wuyafeng <fj970940640@163.com>
2016-06-11 19:45:18 +0800

    Modified Hello.java File

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
 public class Hello
 {
 	public static void main(String args[]){
-		System.out.println("Hello world");
+		System.out.println("Happy new year");
+		System.out.println("Happy everybody");
 	}
 }
